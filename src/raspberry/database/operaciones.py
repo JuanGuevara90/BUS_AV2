693e1e1b27c67c2ff8056ece5898e6a8be5656b1
JuanGuevara90 <guevara_juan1990@hotmail.com>
2022-12-10 16:44:35 -0500

    fix operaciones.py

--- src/raspberry/database/operaciones.py
+++ src/raspberry/database/operaciones.py
@@ -1,15 +1,6 @@
 from array import array
 import datetime 
 
-from conexion import create_connection
-
-conn = create_connection()
-
-def getDate_Current():
-    currentDatetime = datetime.datetime.now()
-    formatDate = currentDatetime.strftime("%y-%m-%d")
-    return formatDate
-
 def getDatosActuales(conn,fechaActual):
     try:
         cur = conn.cursor()
@@ -22,10 +13,8 @@ def getDatosActuales(conn,fechaActual):
             arrayData.append(i[1])
             arrayData.append(i[2])
             arrayData.append(i[3])
-        conn.close()
         return arrayData
     except:
-        conn.close()
         print("Error GetDatos actuales")
     
 def getRoutes(conn):
@@ -37,10 +26,8 @@ def getRoutes(conn):
         for i in cur.fetchall():
             arrayData.append(i[0])
             arrayData.append(i[1])
-        conn.close()
         return arrayData
     except:
-        conn.close()
         print("Error GetRoutes")
 
 def ingresarRegistroPasajeros(conn,fechaActual):
@@ -50,9 +37,7 @@ def ingresarRegistroPasajeros(conn,fechaActual):
         sql_insert = "INSERT INTO Registro_Pasajeros (Fecha,Total_PasajerosActual,Total_Pasajeros,Aforo) VALUES ('"+fechaActual+"','1','1','20')"
 
         cur.execute(sql_insert)
-        conn.close()
     except:
-        conn.close()
         print("Error ingresarRegistroPasajeros")
         
 def ingresarRutaBus(conn):
@@ -60,9 +45,7 @@ def ingresarRutaBus(conn):
         cur = conn.cursor()
         sql_insert = "INSERT INTO Bus (origen,destino) VALUES ('Ibarra','Atuntaqui')"
         cur.execute(sql_insert)
-        conn.close()
     except:
-        conn.close()
         print("Error ingresarRutaBus")
     
 def actualizarRegistroSuma(conn,fechaActual):
@@ -70,9 +53,7 @@ def actualizarRegistroSuma(conn,fechaActual):
         cur = conn.cursor()
         sql_update ="update  Registro_Pasajeros set Total_PasajerosActual=Total_PasajerosActual+1 ,Total_Pasajeros=Total_Pasajeros+1 where Fecha='"+fechaActual+"'"
         cur.execute(sql_update)
-        conn.close()
     except:
-        conn.close()
         print("Error actualizarRegistroSuma")
 
 def actualizarRegistroResta(conn,fechaActual):
@@ -80,9 +61,7 @@ def actualizarRegistroResta(conn,fechaActual):
         cur = conn.cursor()
         sql_update ="update  Registro_Pasajeros set Total_PasajerosActual=Total_PasajerosActual-1 where Fecha='"+fechaActual+"'"
         cur.execute(sql_update)
-        conn.close()
     except:
-        conn.close()
         print("Error actualizarRegistroResta")
     
 def disponibilidadBus(conn,fechaActual):
@@ -94,11 +73,9 @@ def disponibilidadBus(conn,fechaActual):
             Total_PasajerosActual=i[0]
             aforo=i[1]
             if (Total_PasajerosActual<aforo):
-                conn.close()
                 return True
             return False
     except:
-        conn.close()
         print("Error disponibilidadBus")
     
 def validateLeft(conn,fechaActual):
@@ -109,27 +86,21 @@ def validateLeft(conn,fechaActual):
         for i in cur.fetchall():
             Total_PasajerosActual=i[0]
             if (Total_PasajerosActual>0):
-                conn.close()
                 return True
             return False
     except:
-        conn.close()
         print("Error validateLeft")
 
 def existeRegistrosFechaActual(conn,fechaActual):
     try:
         cur = conn.cursor()
-        print(fechaActual)
         sql_query= "SELECT COUNT(*) FROM Registro_Pasajeros WHERE Fecha='"+fechaActual+"'"
-        print(sql_query)
         cur.execute(sql_query)
         for i in cur.fetchall():
             if (i[0]>0):
-                conn.close()
                 return True
             return False
     except:
-        conn.close()
         print("error existeRegistrosFechaActual")
 
 
@@ -140,6 +111,3 @@ def Asientosdisponibles(array):
     except:
         print("Error Asientosdisponibles")
 
-print(getRoutes(conn))
-
-existeRegistrosFechaActual(conn,getDate_Current())
