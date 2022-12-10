07f40bc620f0da168e011d5fef9a9f2121364f23
JuanGuevara90 <guevara_juan1990@hotmail.com>
2022-12-10 16:38:59 -0500

    postgresql

--- src/raspberry/database/operaciones.py
+++ src/raspberry/database/operaciones.py
@@ -1,108 +1,145 @@
 from array import array
-from ast import Try
-from sqlite3 import Error
+import datetime 
+
+from conexion import create_connection
+
+conn = create_connection()
+
+def getDate_Current():
+    currentDatetime = datetime.datetime.now()
+    formatDate = currentDatetime.strftime("%y-%m-%d")
+    return formatDate
 
 def getDatosActuales(conn,fechaActual):
     try:
+        cur = conn.cursor()
         sql_query= "select Fecha,Total_PasajerosActual,Total_Pasajeros, Aforo from Registro_Pasajeros WHERE Fecha='"+fechaActual+"'"
 
-        cursor=conn.execute(sql_query)
+        cur.execute(sql_query)
         arrayData =[]
-        for i in cursor:
+        for i in cur.fetchall():
             arrayData.append(i[0])
             arrayData.append(i[1])
             arrayData.append(i[2])
             arrayData.append(i[3])
+        conn.close()
         return arrayData
-    except Error as e:
-        print(e)
+    except:
+        conn.close()
+        print("Error GetDatos actuales")
     
 def getRoutes(conn):
     try:
+        cur = conn.cursor()
         sql_query= "select origen,destino from Bus"
-        cursor=conn.execute(sql_query)
+        cur.execute(sql_query)
         arrayData =[]
-        for i in cursor:
+        for i in cur.fetchall():
             arrayData.append(i[0])
             arrayData.append(i[1])
+        conn.close()
         return arrayData
-    except Error as e:
-        print(e)
+    except:
+        conn.close()
+        print("Error GetRoutes")
 
 def ingresarRegistroPasajeros(conn,fechaActual):
     try:
+        cur = conn.cursor()
 
         sql_insert = "INSERT INTO Registro_Pasajeros (Fecha,Total_PasajerosActual,Total_Pasajeros,Aforo) VALUES ('"+fechaActual+"','1','1','20')"
 
-        conn.execute(sql_insert)
-        conn.commit()
-    except Error as e:
-        print(e)
+        cur.execute(sql_insert)
+        conn.close()
+    except:
+        conn.close()
+        print("Error ingresarRegistroPasajeros")
         
 def ingresarRutaBus(conn):
     try:
+        cur = conn.cursor()
         sql_insert = "INSERT INTO Bus (origen,destino) VALUES ('Ibarra','Atuntaqui')"
-        conn.execute(sql_insert)
-        conn.commit()
-    except Error as e:
-        print(e)
+        cur.execute(sql_insert)
+        conn.close()
+    except:
+        conn.close()
+        print("Error ingresarRutaBus")
     
 def actualizarRegistroSuma(conn,fechaActual):
     try:
+        cur = conn.cursor()
         sql_update ="update  Registro_Pasajeros set Total_PasajerosActual=Total_PasajerosActual+1 ,Total_Pasajeros=Total_Pasajeros+1 where Fecha='"+fechaActual+"'"
-        conn.execute(sql_update)
-        conn.commit()
-    except Error as e:
-        print(e)
+        cur.execute(sql_update)
+        conn.close()
+    except:
+        conn.close()
+        print("Error actualizarRegistroSuma")
 
 def actualizarRegistroResta(conn,fechaActual):
     try:
+        cur = conn.cursor()
         sql_update ="update  Registro_Pasajeros set Total_PasajerosActual=Total_PasajerosActual-1 where Fecha='"+fechaActual+"'"
-        conn.execute(sql_update)
-        conn.commit()
-    except Error as e:
-        print(e)
+        cur.execute(sql_update)
+        conn.close()
+    except:
+        conn.close()
+        print("Error actualizarRegistroResta")
     
 def disponibilidadBus(conn,fechaActual):
     try:
+        cur = conn.cursor()
         sql_query= "select Total_PasajerosActual, Aforo from Registro_Pasajeros WHERE Fecha='"+fechaActual+"'"
-        cursor=conn.execute(sql_query)
-        for i in cursor:
+        cur.execute(sql_query)
+        for i in cur.fetchall():
             Total_PasajerosActual=i[0]
             aforo=i[1]
             if (Total_PasajerosActual<aforo):
+                conn.close()
                 return True
             return False
-    except Error as e:
-        print(e)
+    except:
+        conn.close()
+        print("Error disponibilidadBus")
     
 def validateLeft(conn,fechaActual):
     try:
+        cur = conn.cursor()
         sql_query= "select Total_PasajerosActual, Aforo from Registro_Pasajeros WHERE Fecha='"+fechaActual+"'"
-        cursor=conn.execute(sql_query)
-        for i in cursor:
+        cur.execute(sql_query)
+        for i in cur.fetchall():
             Total_PasajerosActual=i[0]
             if (Total_PasajerosActual>0):
+                conn.close()
                 return True
             return False
-    except Error as e:
-        print(e)
+    except:
+        conn.close()
+        print("Error validateLeft")
 
 def existeRegistrosFechaActual(conn,fechaActual):
     try:
-        sql_query= "select count(*) from Registro_Pasajeros WHERE Fecha='"+fechaActual+"'"
-        cursor=conn.execute(sql_query)
-        for i in cursor:
+        cur = conn.cursor()
+        print(fechaActual)
+        sql_query= "SELECT COUNT(*) FROM Registro_Pasajeros WHERE Fecha='"+fechaActual+"'"
+        print(sql_query)
+        cur.execute(sql_query)
+        for i in cur.fetchall():
             if (i[0]>0):
+                conn.close()
                 return True
             return False
-    except Error as e:
-        print(e)
+    except:
+        conn.close()
+        print("error existeRegistrosFechaActual")
 
 
 def Asientosdisponibles(array):
     try:
         free =array[3]-array[1]
         return free 
-    except Error as e:
-        print(e)
+    except:
+        print("Error Asientosdisponibles")
+
+print(getRoutes(conn))
+
+existeRegistrosFechaActual(conn,getDate_Current())
